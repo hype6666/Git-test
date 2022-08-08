139db05a3fa6c49564dd63dc2ba3b9ac4f4568f5
GGMU <GGMU@126.com>
2022-08-08 13:58:44 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.xml.transform.Source;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
